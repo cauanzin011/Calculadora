cc8eb63a2c16768d07488190bb1cc98b41f389a7
cauanzin011 <cauadamasceno2011@gmail.com>
2026-05-09 13:32:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+let calculo = "";
+let localConta =
+ document.getElementById("localConta");
+const principal =
+ document.getElementById("principal");
+function clicar(valor){
+ let ultimo = calculo[calculo.length - 1];
+ if(ultimo === "+" && valor === "+"){
+  return;
+ }
+ else if(ultimo === "-" && valor === "-"){
+  return;
+ }
+ else if(ultimo === "*" && valor === "×"){
+  return;
+ }
+ else if(ultimo === "/" && valor === "÷"){
+  return;
+ }
+  else if(ultimo === "." && valor === ","){
+  return;
+ }
+ else if(ultimo === "*" && valor === "P"){
+  return;
+ }
+ if (valor === "×"){
+  calculo += "*"
+  localConta.innerText += "×"
+}else if (valor === "÷"){
+  calculo += "/"
+ localConta.innerText += "÷"
+}
+    else if (valor === "P"){
+        calculo += "**"
+        localConta.innerText += "°"
+    }
+    else if (valor === ","){
+        calculo += "."
+        localConta.innerText +=","
+    }
+ else{
+  calculo += valor;
+  localConta.innerText += valor
+}
+}
+function calcular(){
+ calculo = eval(calculo)
+ localConta.textContent = (calculo);
+}
+function limpar(){
+ calculo = "";
+ localConta.textContent = "";
+}
+function porcentagem(){
+    calculo = eval(calculo) / 100;
+    localConta.textContent = calculo;
+}
+function apagar(){
+    calculo = calculo.slice(0, -1);
+    localConta.textContent = 
+    localConta.textContent.slice(0, -1);
+}
